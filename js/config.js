2e06d55570016e58f869e36148afd693347cc56e
dianiluz <dianiluz05@gmail.com>
2026-03-09 20:34:51 -0500

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,5 +1,5 @@
 const CUPISSA_CONFIG = {
-    API_URL: "https://script.google.com/macros/s/AKfycbz0IEEXDtWHyN0EAks9WAxXAni6B9agd29VVfIEUaLTMWpMgaTmc_gtBVfHgEncA70/exec",
+    API_URL: "https://script.google.com/macros/s/AKfycbz0lEEXDtWHyN0EAks9WAxXAni6B9agd29VVflEUaLTMWpMgaTmc_gtBVfHgEncA70/exec",
     COLORS: {
         FUCSIA: "#db137a",
         BLANCO: "#ffffff",

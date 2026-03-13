4c588a9e651be7f5ec21058964d228547bafc954
dianiluz <dianiluz05@gmail.com>
2026-03-13 16:41:43 -0500

    Create settings.json

--- js/config.js
+++ js/config.js
@@ -1,5 +1,19 @@
 const CUPISSA_CONFIG = {
+    // Tu nueva bóveda de Apps Script (Backend)
     API_URL: "https://script.google.com/macros/s/AKfycbz0lEEXDtWHyN0EAks9WAxXAni6B9agd29VVflEUaLTMWpMgaTmc_gtBVfHgEncA70/exec",
+    
+    // ¡EL TOKEN YA NO ESTÁ AQUÍ! Ahora está seguro en el backend.
+    github: {
+        owner: "dianiluz", 
+        repo: "cupissa", 
+        branch: "main"
+    },
+  
+    supabase: {
+        url: "https://dhsoimmxfapcppdyjhcj.supabase.co",
+        key: "sb_publishable__F5Xz9UmI0-HOreX3MhFqA_9eZX0qRJ"
+    },
+
     COLORS: {
         FUCSIA: "#db137a",
         BLANCO: "#ffffff",

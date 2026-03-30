2437348635c345800963ea51d3e2598e4ef0eee7
dianiluz <dianiluz05@gmail.com>
2026-03-30 16:58:11 -0500

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 const CUPISSA_CONFIG = {
     // Tu nueva bóveda de Apps Script (Backend)
     API_URL: "https://script.google.com/macros/s/AKfycbz0lEEXDtWHyN0EAks9WAxXAni6B9agd29VVflEUaLTMWpMgaTmc_gtBVfHgEncA70/exec",
-    
+    API_TOKEN: "Deiner123.Cupissa_2026_xYz987!"
     // ¡EL TOKEN YA NO ESTÁ AQUÍ! Ahora está seguro en el backend.
     github: {
         owner: "dianiluz", 
